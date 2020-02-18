a81ae6108f833b5f336c2776ac0c212b921f52d9
Luis Arana <luarana631@gmail.com>
2020-02-18 16:46:21 -0600

    Insertar registros

--- main.cpp
+++ main.cpp
@@ -9,18 +9,11 @@ using namespace std;
 void menu_Inicial();
 bool validar_Entrada(string);
 void ingresar_Registro();
+void reporte_Registro();
+void editar_Registro();
 
-struct estudianteStruct{
-    char tipo[1];
-    char apellido1[21];
-    char apellido2[21];
-    char nombre[21];
-    char codigo[11];
-    int seccion;
-};
-
-struct profersorStruct{
-    char tipo[1];
+struct registroStruct{
+    int tipo;
     char apellido1[21];
     char apellido2[21];
     char nombre[21];
@@ -62,14 +55,15 @@ void menu_Inicial(){
     case 1:
         //Ingresar
         ingresar_Registro();
+        menu_Inicial();
         break;
     case 2:
         //Editar
-
+        editar_Registro();
         break;
     case 3:
-        //Eliminar
-
+        //Reporte
+        reporte_Registro();
         break;
     default:
         cout<<"-------------Opcion no existe--------------"<<endl;
@@ -79,13 +73,72 @@ void menu_Inicial(){
     }
 }
 
+void editar_Registro(){
+    registroStruct registro;
+    FILE* archivo;
+
+    char apellido1[21], apellido2[21], nombre[21], codigo[11];
+    int tipo, seccion;
+
+    if ((archivo = fopen("alumnos_201700988.bin","rb+")) == NULL){
+           cout<<"Error al abrir el archivo"<<endl;
+           exit(1);
+    }
+
+}
+
 void ingresar_Registro(){
+    registroStruct registro;
+    FILE* archivo;
+
+    char apellido1[21], apellido2[21], nombre[21], codigo[11];
+    int tipo, seccion;
+
+    if ((archivo = fopen("alumnos_201700988.bin","rb+")) == NULL){
+           cout<<"Error al abrir el archivo"<<endl;
+           exit(1);
+    }
+
+    cout<<"Ingrese los datos del registro: "<<endl;
+    cout<<"tipo: ";
+    cin>>tipo;
+    registro.tipo = tipo;
+    cout<<"apellido 1: ";
+    cin>>apellido1;
+    strcpy(registro.apellido1,apellido1);
+    cout<<"apellido 2: ";
+    cin>>apellido2;
+    strcpy(registro.apellido2,apellido2);
+    cout<<"nombre: ";
+    cin>>nombre;
+    strcpy(registro.nombre, nombre);
+    cout<<"codigo: ";
+    cin>>codigo;
+    strcpy(registro.codigo, codigo);
+    cout<<"seccion: ";
+    cin>>seccion;
+    registro.seccion = seccion;
+
+    fseek(archivo,0,SEEK_END);
+    fwrite(&registro, sizeof(registroStruct),1,archivo);
+    fclose(archivo);
+}
+
+void reporte_Registro(){
+    registroStruct registro;
+    FILE* archivo;
+
+    if ((archivo = fopen("alumnos_201700988.bin","rb")) == NULL){
+           cout<<"Error al abrir el archivo"<<endl;
+           exit(1);
+    }
+
 
 }
 
 void crear_Archivo(){
     ofstream archivo;
-    archivo.open("alumnos_201700988.dat",ios::binary);
+    archivo.open("alumnos_201700988.bin",ios::binary);
 
     if(archivo.fail()){
         cout<<"No se pudo crear el archivo."<<endl;
